8e3140933d406c15703e409800f60579c825308c
Dean Janssens <djanssens@fullsail.edu>
2014-06-18 07:36:56 -0500

    added first variables and 1 function

--- janssens_dean_assignment3.js
+++ janssens_dean_assignment3.js
@@ -6,7 +6,28 @@
 //June 19, 2014
 //Project 3
 
-var noIdea = "I Have no idea"
-var number
-var boolean
+
+
+var sauteIngredients = ["1 lb cooked chicken",
+                   "1 cup diced green pepper",
+                   "1 cup diced white onion",
+                   "2 teaspoons chili powder",
+                   "2 cloves garlic, minced"];
+var cookingTime = 35;
+var sauteing = true;
+
+
+
+//Array Function
+
+var firstStep  = function (firstIngredients){
+    var lastIngredient = sauteIngredients.pop();
+    console.log("In frying pan add " + firstIngredients + " and " + lastIngredient + ".");
+}
+
+firstStep(sauteIngredients);
+
+
+
+
 
