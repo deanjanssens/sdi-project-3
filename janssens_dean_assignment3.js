c96b6266587bd4806033191170a22e2c9475281f
Dean Janssens <djanssens@fullsail.edu>
2014-06-19 11:19:09 -0500

    added procedure and json data

--- janssens_dean_assignment3.js
+++ janssens_dean_assignment3.js
@@ -8,24 +8,46 @@
 
 
 
-var sauteIngredients = ["1 lb cooked chicken",
+var sauteIngredients = ["1 lb cooked diced chicken",
                    "1 cup diced green pepper",
                    "1 cup diced white onion",
                    "2 teaspoons chili powder",
-                   "2 cloves garlic, minced"];
-var cookingTime = 35;
-var sauteing = true;
+                   "1 Tablespoon minced garlic"];
 
+var totalEnchiladas = 12
+var bakingTime = 35
+var suppliesSetUp = confirm("Are all your ingredients and cooking supplies ready?");
+
+//Procedure
+var suppliesReady = function (suppliesSetUp){
+    var answer = suppliesSetUp;
+    if (answer === true) {
+        console.log("You are ready to make the enchiladas.");
+    } else if (answer === false) {   
+        console.log("Please gather all ingredients and cooking supplies before beginning.");
+    
+    }
+    
+};
 
 
 //Array Function
-
-var firstStep  = function (firstIngredients){
-    var lastIngredient = sauteIngredients.pop();
-    console.log("In frying pan add " + firstIngredients + " and " + lastIngredient + ".");
-}
-
+  var firstStep = function (firstIngredients) {
+    var lastIngredient = sauteIngredients.pop();      
+    console.log("The first ingredients are " + firstIngredients + ", and " + lastIngredient + ".");
+    sauteIngredients.push("1 Tablespoon minced garlic");
+    for (var i = 0; i < sauteIngredients.length; i++) {
+        console.log("Added " + firstIngredients[i] + " to frying pan.");
+    };
+    console.log("Time to saute!");
+    return firstIngredients;
+  };
+//Main Code
+suppliesReady(suppliesSetUp);
 firstStep(sauteIngredients);
+
+
+
 
 
 
--- json.js
+++ json.js
@@ -6,5 +6,27 @@
 //June 19, 2014
 //Project 3
 
+var dinnerGuests = {
+    "guests": [
+        {
+            "dinnerGuest1" : "Jim",
+            "favoriteDrink" : "Ice Tea",
+            
+            
+        },
+        {
+            "dinnerGuest2" : "Karen",
+            "favoriteDrink" : "Coke",
+            
+        },
+        {
+             "dinnerGuest3" : "George",
+             "favoriteDrink" : "Cherry Limeade",         
+        }
+        
+    ]
+    
+};
+
 
 
